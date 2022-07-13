240653c60a111a7c57e197fb63a0544635bffb1e
Gabriel Dürr <gabriel.durr@outlook.com>
2022-07-13 17:16:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {MovieCard} from "./components/MovieCard";
 
 import {SideBar} from "./components/SideBar";
 import {Content} from "./components/Content";
-
+//..
 import {api} from "./services/api";
 
 import "./styles/global.scss";
